fe116e99f276735b7d699a98ac0d9dd1b4a6021b
uidevperson <uidevthing@gmail.com>
2017-12-21 15:53:22 +0000

    testing

---
+++ app/components/examples.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'React';
+// import { Field, reduxForm } from 'redux-form';
+// import { Link } from 'react-router-dom';
+// import { connect } from 'react-redux';
+// import { createPost } from '../actions';
+
+export default class Examples extends Component {
+
+		render() {
+			return (
+				<div>Examples</div>
+			);
+		}
+	}
+
--- app/index.js
+++ app/index.js
@@ -1,17 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'bootstrap';
-import Holder from 'holderjs';
-import $ from 'jquery';
+import Holder from 'holderjs'; // TODO remove/replace
 // styling 
 import './styles/index.scss';
-
+// NO IDEA WHY THIS IS BEING USED FOR
 /*
  * I didn't use arrow callback because in the current version of webpack
  * on production build it returns an error.
  */
-$(function onReady() { // eslint-disable-line prefer-arrow-callback
-  Holder.addTheme('thumb', {
-    background: '#55595c',
-    foreground: '#eceeef',
-    text: 'Thumbnail',
-  });
-});
+// $(function onReady() { // eslint-disable-line prefer-arrow-callback
+//   Holder.addTheme('thumb', {
+//     background: '#55595c',
+//     foreground: '#eceeef',
+//     text: 'Thumbnail',
+//   });
+// });
